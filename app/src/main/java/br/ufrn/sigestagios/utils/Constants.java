c44612bba9e0734d5e9f9a0f77eb0839f6de4d34
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2017-10-26 20:35:00 -0300

    Login screen and authentication

---
+++ app/src/main/java/br/ufrn/sigestagios/utils/Constants.java
@@ -0,0 +1,11 @@
+package br.ufrn.sigestagios.utils;
+
+/**
+ * Created by YuriAlessandro on 26/10/2017.
+ */
+
+public class Constants {
+    public static final String KEY_ACCESS_TOKEN = "access_token";
+    public static final String KEY_REFRESH_TOKEN = "refresh_token";
+    public static final String KEY_EXPIRES_IN = "expires_in";
+}
