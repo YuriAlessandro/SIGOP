ec5a930d2e61aae62f5d2f9bbbdd16f2ef980844
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2017-10-27 17:28:28 -0300

    Get logged user name; Logout

--- app/src/main/java/br/ufrn/sigestagios/utils/Constants.java
+++ app/src/main/java/br/ufrn/sigestagios/utils/Constants.java
@@ -8,4 +8,6 @@ public class Constants {
     public static final String KEY_ACCESS_TOKEN = "access_token";
     public static final String KEY_REFRESH_TOKEN = "refresh_token";
     public static final String KEY_EXPIRES_IN = "expires_in";
+
+    public static final String URL_BASE = "https://apitestes.info.ufrn.br/";
 }
