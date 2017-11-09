35cbcb1a9eee4dd9f4540aa0e122decdc61269cb
joaoei <joaoeizidio@gmail.com>
2017-11-08 23:57:38 -0300

    Database ok, but api request is the problem now

--- app/src/main/java/br/ufrn/sigestagios/database/OfferDatabase.java
+++ app/src/main/java/br/ufrn/sigestagios/database/OfferDatabase.java
@@ -12,12 +12,12 @@ import br.ufrn.sigestagios.database.OfferDBContract.OfferEntry;
 public class OfferDatabase extends SQLiteOpenHelper {
 
     private static final String NOME_BANCO = "Ofertas";
-    private static final int VERSAO = 1;
+    private static final int VERSAO = 2;
 
     private static final String SQL_CREATE_ENTRIES =
             "CREATE TABLE " + OfferEntry.TABLE_NAME + " ("
-            + OfferEntry._ID + " INTEGER PRIMARY KEY, " + OfferEntry.DESCRICAO + ", " + OfferEntry.UNIDADE + ", " + OfferEntry.ID_UNIDADE
-            + ", " + OfferEntry.EMAIL +  ")";
+                    + OfferEntry._ID + " INTEGER PRIMARY KEY, " + OfferEntry.DESCRICAO + ", " + OfferEntry.UNIDADE + ", " + OfferEntry.ID_UNIDADE
+                    + ", " + OfferEntry.EMAIL +  ")";
 
     private static final String SQL_DELETE_ENTRIES =
             "DROP TABLE IF EXISTS " + OfferEntry.TABLE_NAME;
@@ -33,6 +33,6 @@ public class OfferDatabase extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        // nada ainda
+        db.execSQL(SQL_DELETE_ENTRIES);
     }
 }
