250bedb2007c5f4f9b572b70302f41bc258f985f
joaoei <joaoeizidio@gmail.com>
2017-12-02 00:28:24 -0300

    create activities detail offers #20

--- app/src/main/java/br/ufrn/sigestagios/models/AssociatedAction.java
+++ app/src/main/java/br/ufrn/sigestagios/models/AssociatedAction.java
@@ -8,12 +8,12 @@ import java.io.Serializable;
 
 public class AssociatedAction extends Offer implements Serializable {
     private int year;
-    private String cpf_cnpj;
+    private int cpf_cnpj;
     private String responsible;
     private int positionsRemunerated;
     private int idProject;
 
-    public AssociatedAction(String description, String term, int idTerm, String email, int year, String cpf_cnpj, String responsible, int positionsRemunerated, int idProject) {
+    public AssociatedAction(String description, String term, int idTerm, String email, int year, int cpf_cnpj, String responsible, int positionsRemunerated, int idProject) {
         super(description, term, idTerm, email);
         this.year = year;
         this.cpf_cnpj = cpf_cnpj;
@@ -26,7 +26,7 @@ public class AssociatedAction extends Offer implements Serializable {
         return year;
     }
 
-    public String getCpf_cnpj() {
+    public int getCpf_cnpj() {
         return cpf_cnpj;
     }
 
