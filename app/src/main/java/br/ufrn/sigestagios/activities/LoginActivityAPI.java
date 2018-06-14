57a1a69206f1f72ebda84f9d13b15bf12162724b
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2018-06-14 14:28:03 -0300

    Login activity

---
+++ app/src/main/java/br/ufrn/sigestagios/activities/LoginActivityAPI.java
@@ -0,0 +1,15 @@
+package br.ufrn.sigestagios.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import br.ufrn.sigestagios.R;
+
+public class LoginActivityAPI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_api);
+    }
+}
