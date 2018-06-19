27fa6e3a5dc35ecae34b8006c4933a0cd387c689
Yuri Alessandro Martins <yuri.alessandro@hotmail.com>
2018-06-19 20:04:16 -0300

    add floating button

--- app/src/main/java/br/ufrn/sigestagios/activities/OfferActivity.java
+++ app/src/main/java/br/ufrn/sigestagios/activities/OfferActivity.java
@@ -6,7 +6,9 @@ import android.content.res.ColorStateList;
 import android.database.Cursor;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
@@ -151,6 +153,16 @@ public class OfferActivity extends AppCompatActivity {
             public void onRefresh() {
                 refreshItems();
                 mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getApplicationContext(), RegistrationFormActivity.class);
+                i.putExtra("currentUserId", loggedUser.getUserId());
+                startActivityForResult(i, REGISTER);
             }
         });
 
